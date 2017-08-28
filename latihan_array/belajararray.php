6af30b9f072a103237e2fceb069355a4588b8b7a
unknown <muhammadsirajuddin39@gmail.com>
2017-08-29 03:48:20 +0700

    belajar

---
+++ latihan_array/belajararray.php
@@ -0,0 +1,5 @@
+<?php
+	$ayam = array ("kate", "bangkok", "kampung", "ayam bakar", "ayam goreng", "ayam panggang");
+	
+	echo "macam-macam ayam: " . $ayam[0] . ", " . $ayam[1] . ", " . $ayam[2] . ", " . $ayam[3] . ", " . $ayam[3] . ", " . $ayam[4] . ", " . $ayam[5] . ".";
+?>
